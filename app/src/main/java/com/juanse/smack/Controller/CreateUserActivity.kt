2d12f9209d607a45c33a813232328c9c83bf68fc
Juansebar <juansebar1991@gmail.com>
2018-05-31 12:17:07 -0400

    Implemented createUser Integration saving username, avatar details

--- app/src/main/java/com/juanse/smack/Controller/CreateUserActivity.kt
+++ app/src/main/java/com/juanse/smack/Controller/CreateUserActivity.kt
@@ -9,6 +9,7 @@ import android.view.View
 import android.widget.Toast
 import com.juanse.smack.R
 import com.juanse.smack.Services.AuthService
+import com.juanse.smack.Services.UserDataService
 import kotlinx.android.synthetic.main.activity_create_user.*
 import java.util.*
 
@@ -57,28 +58,42 @@ class CreateUserActivity : AppCompatActivity() {
 
     private fun onCreateUserClick() {
         // Extract input
+        val username: String? = signUpUsernameText.text.toString().takeIf { it.isNotEmpty() }
         val email: String? = signUpEmailText.text.toString().takeIf { it.isNotEmpty() } ?: null
         val password: String? = signUpPasswordText.text.toString().takeIf { it.isNotEmpty() } ?:  null
 
-        var emailValid = email != null
-        var passwordValid = password != null
+        val emailValid = email != null
+        val passwordValid = password != null
+        val usernameValid = username != null
 
         // Validation
-        if (!emailValid && !passwordValid) {
-            showError(signUpCreateUserButton, "Must enter a valid email and password")
+        if (!emailValid && !passwordValid && !usernameValid) {
+            showError(signUpCreateUserButton, "Must enter a valid username, email and password")
         } else if (!emailValid) {
             showError(signUpCreateUserButton, "Must enter an email")
         } else if (!passwordValid) {
             showError(signUpCreateUserButton, "Must enter a password")
+        } else if (!usernameValid) {
+            showError(signUpCreateUserButton, "Must enter a username")
         }
 
-        if (emailValid && passwordValid) {
-            AuthService.registerUser(this, email!!.toString(), password!!.toString()) { success ->
+        if (emailValid && passwordValid && usernameValid) {
+            AuthService.registerUser(this, email!!, password!!) { success ->
                 if (success) {
                     Toast.makeText(this, "Succesfully Registered User", Toast.LENGTH_SHORT).show()
-                    AuthService.loginUser(this, email!!.toString(), password!!.toString()) {loginSuccess ->
+
+                    AuthService.loginUser(this, email, password) { loginSuccess ->
                         if (loginSuccess) {
                             Toast.makeText(this, "Successfully logged in user", Toast.LENGTH_SHORT).show()
+
+                            AuthService.createUser(this, username!!, email, userAvatar, avatarColor) { success ->
+                                if (success) {
+                                    Toast.makeText(this, "User created!", Toast.LENGTH_SHORT).show()
+                                    finish() // Dismisses an activity
+                                } else {
+                                    Toast.makeText(this, "User creation FAILED!", Toast.LENGTH_SHORT).show()
+                                }
+                            }
                         } else {
                             Toast.makeText(this, "Couldn't login, please try again", Toast.LENGTH_SHORT).show()
                         }
--- app/src/main/java/com/juanse/smack/Services/AuthService.kt
+++ app/src/main/java/com/juanse/smack/Services/AuthService.kt
@@ -7,6 +7,7 @@ import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import com.juanse.smack.Utilities.URL_CREATE_USER
 import com.juanse.smack.Utilities.URL_LOGIN
 import com.juanse.smack.Utilities.URL_REGISTER
 import org.json.JSONException
@@ -72,7 +73,6 @@ object AuthService {
             Log.d("ERROR", "Could not login user: $error")
             completion(false)
         }) {
-            // Headers
             override fun getBodyContentType(): String {
                 return "application/json; charset=utf-8"
             }
@@ -83,6 +83,50 @@ object AuthService {
         }
 
         Volley.newRequestQueue(context).add(loginRequest)
+    }
+
+    fun createUser(context: Context, username: String, email: String, avatarName: String, avatarColor: String, completion: (Boolean) -> Unit) {
+        val jsonBody = JSONObject()
+        jsonBody.put("username", username)
+        jsonBody.put("email", email)
+        jsonBody.put("avatarName", avatarName)
+        jsonBody.put("avatarColor", avatarColor)
+        val requestBody = jsonBody.toString()
+
+        val createUserRequest = object : JsonObjectRequest(Method.POST, URL_CREATE_USER, null, Response.Listener { response ->  
+            println("RESPONSE: $response")
+            try {
+                UserDataService.id = response.getString("_id")
+                UserDataService.email = response.getString("email")
+                UserDataService.avatarName = response.getString("avatarName")
+                UserDataService.avatarColor = response.getString("avatarColor")
+
+                completion(true)
+            } catch (error: JSONException) {
+                Log.d("ERROR", "EXC: " + error.localizedMessage)
+                completion(false)
+            }
+        }, Response.ErrorListener { error ->
+            Log.d("ERROR", "Could not create user: $error")
+            completion(false)
+        }) {
+            override fun getBodyContentType(): String {
+                return "application/json; charset=utf-8"
+            }
+
+            override fun getBody(): ByteArray {
+                return requestBody.toByteArray()
+            }
+
+            override fun getHeaders(): MutableMap<String, String> {
+//                val headers = HashMap<String, String>()
+//                headers.put("Authorization", "Bearer $authToken")
+
+                return hashMapOf("Authorization" to "Bearer $authToken")
+            }
+        }
+
+        Volley.newRequestQueue(context).add(createUserRequest)
     }
 
 }
